9feabf15213a6339f45a853e24ab278b085ea436
mshanemc <shane.mclaughlin@salesforce.com>
2022-12-05 13:11:00 -0600

    test for new non-migration rules

--- src/rules/commandSummary.ts
+++ src/rules/commandSummary.ts
@@ -4,7 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { ESLintUtils } from '@typescript-eslint/utils';
+import { AST_NODE_TYPES, ESLintUtils } from '@typescript-eslint/utils';
 import { extendsSfCommand, getClassPropertyIdentifierName, isInCommandDirectory } from '../shared/commands';
 export const commandSummary = ESLintUtils.RuleCreator.withoutDocs({
   meta: {
@@ -16,6 +16,7 @@ export const commandSummary = ESLintUtils.RuleCreator.withoutDocs({
       summary: 'Commands should have a summary property',
     },
     type: 'problem',
+    fixable: 'code',
     schema: [],
   },
   defaultOptions: [],
@@ -26,9 +27,24 @@ export const commandSummary = ESLintUtils.RuleCreator.withoutDocs({
             // verify it extends SfCommand
             if (extendsSfCommand(node)) {
               if (!node.body.body.some((member) => getClassPropertyIdentifierName(member) === 'summary')) {
+                const descriptionNode = node.body.body.find(
+                  (member) => getClassPropertyIdentifierName(member) === 'description'
+                );
                 context.report({
                   node: node.id,
                   messageId: 'summary',
+                  // if you have a description, create a summary property with the same value
+                  ...(descriptionNode && descriptionNode.type === AST_NODE_TYPES.PropertyDefinition
+                    ? {
+                        fix: (fixer) =>
+                          fixer.insertTextBefore(
+                            descriptionNode,
+                            `public static readonly summary = ${context
+                              .getSourceCode()
+                              .getText(descriptionNode.value)};`
+                          ),
+                      }
+                    : {}),
                 });
               }
             }
--- test/rules/commandSummary.test.ts
+++ test/rules/commandSummary.test.ts
@@ -68,6 +68,27 @@ export abstract class StagedProgress<T> {
       filename: path.normalize('src/commands/foo.ts'),
       code: `
 export default class EnvCreateScratch extends SfCommand<ScratchCreateResponse> {
+  public static readonly examples = 'baz'
+}
+`,
+    },
+    {
+      name: 'is missing summary but has description so it gets fixed',
+      errors: [
+        {
+          messageId: 'summary',
+        },
+      ],
+      filename: path.normalize('src/commands/foo.ts'),
+      // allow formatter to handle the newline/indent stuff
+      output: `
+export default class EnvCreateScratch extends SfCommand<ScratchCreateResponse> {
+  public static readonly summary = 'bar';public static readonly description = 'bar'
+  public static readonly examples = 'baz'
+}
+`,
+      code: `
+export default class EnvCreateScratch extends SfCommand<ScratchCreateResponse> {
   public static readonly description = 'bar'
   public static readonly examples = 'baz'
 }
