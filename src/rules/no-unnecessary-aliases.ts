db0cb1872fe489a65b37a0e739cba11fbbcafe1f
mshanemc <shane.mclaughlin@salesforce.com>
2023-01-11 09:41:22 -0600

    feat: remove unnecessary command aliases

---
+++ src/rules/no-unnecessary-aliases.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import { AST_NODE_TYPES, ESLintUtils } from '@typescript-eslint/utils';
+import { isInCommandDirectory, ancestorsContainsSfCommand, getCommandNameParts } from '../shared/commands';
+
+export const noUnnecessaryAliases = ESLintUtils.RuleCreator.withoutDocs({
+  meta: {
+    docs: {
+      description:
+        'Mark when an alias is unnecessary because its only an order permutation, not really a different name',
+      recommended: 'error',
+    },
+    messages: {
+      summary: 'the Salesforce CLI will match the command words in any order, so this alias is unnecessary',
+    },
+    type: 'problem',
+    schema: [],
+    fixable: 'code',
+  },
+  defaultOptions: [],
+  create(context) {
+    return isInCommandDirectory(context)
+      ? {
+          Literal(node): void {
+            if (
+              node.parent.type === AST_NODE_TYPES.ArrayExpression &&
+              node.parent.parent.type === AST_NODE_TYPES.PropertyDefinition &&
+              node.parent.parent.key.type === AST_NODE_TYPES.Identifier &&
+              node.parent.parent.key.name === 'aliases' &&
+              ancestorsContainsSfCommand(context.getAncestors())
+            ) {
+              const cmdParts = getCommandNameParts(context.getPhysicalFilename());
+              const aliasParts = typeof node.value === 'string' ? node.value.split(':') : [];
+              if (
+                aliasParts.every((part) => cmdParts.includes(part)) &&
+                cmdParts.every((part) => aliasParts.includes(part))
+              ) {
+                context.report({
+                  node,
+                  messageId: 'summary',
+                  fix: (fixer) => {
+                    const comma = context.getSourceCode().getTokenAfter(node);
+                    return comma.value === ','
+                      ? fixer.removeRange([node.range[0], node.range[1] + 1])
+                      : fixer.remove(node);
+                  },
+                });
+              }
+            }
+          },
+        }
+      : {};
+  },
+});
---
+++ test/rules/no-unnecessary-aliases.test.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import path from 'path';
+import { ESLintUtils } from '@typescript-eslint/utils';
+import { noUnnecessaryAliases } from '../../src/rules/no-unnecessary-aliases';
+
+const ruleTester = new ESLintUtils.RuleTester({
+  parser: '@typescript-eslint/parser',
+});
+
+ruleTester.run('no unnecessary aliases', noUnnecessaryAliases, {
+  valid: [
+    {
+      name: 'aliases are not a permutation',
+      filename: path.normalize('src/commands/foo/bar/baz.ts'),
+      code: `
+export default class EnvCreateScratch extends SfCommand<ScratchCreateResponse> {
+  public static readonly aliases = ['bar:baz', 'bar:qux'];
+}
+`,
+    },
+
+    {
+      name: 'not in commands dir',
+      filename: path.normalize('src/shared/foo/bar/baz.ts'),
+      code: `
+export default class EnvCreateScratch extends SfCommand<ScratchCreateResponse> {
+  public static readonly aliases = ['foo:baz:bar'];
+}
+`,
+    },
+  ],
+  invalid: [
+    {
+      name: 'one alias is a permutation, but there is another that is not',
+      errors: [
+        {
+          messageId: 'summary',
+        },
+      ],
+      filename: path.normalize('src/commands/foo/bar/baz.ts'),
+      code: `
+export default class EnvCreateScratch extends SfCommand<ScratchCreateResponse> {
+  public static readonly aliases = ['bar:baz:foo', 'bar:qux'];
+}
+`,
+      output: `
+export default class EnvCreateScratch extends SfCommand<ScratchCreateResponse> {
+  public static readonly aliases = [ 'bar:qux'];
+}
+`,
+    },
+  ],
+});
