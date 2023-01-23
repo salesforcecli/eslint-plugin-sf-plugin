3c56c593db2ba447a862e24c6d7dd4de7e9f07a4
mshanemc <shane.mclaughlin@salesforce.com>
2023-01-23 09:17:55 -0600

    feat: strict-false checks, docs catchup

---
+++ src/rules/no-args-parse-without-strict-false.ts
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { AST_NODE_TYPES, ESLintUtils, ASTUtils } from '@typescript-eslint/utils';
+import { isInCommandDirectory, getSfCommand } from '../shared/commands';
+
+export const noArgsParseWithoutStrictFalse = ESLintUtils.RuleCreator.withoutDocs({
+  meta: {
+    docs: {
+      description: 'If you parse args/argv, the class should have strict set to false',
+      recommended: 'error',
+    },
+    messages: {
+      summary: 'If you parse args/argv, the class should have strict set to false',
+    },
+    type: 'problem',
+    schema: [],
+    fixable: 'code',
+  },
+  defaultOptions: [],
+  create(context) {
+    return isInCommandDirectory(context)
+      ? {
+          // eslint-disable-next-line complexity
+          VariableDeclarator(node): void {
+            if (
+              ASTUtils.isAwaitExpression(node.init) &&
+              node.init.argument.type === AST_NODE_TYPES.CallExpression &&
+              node.init.argument.callee.type === AST_NODE_TYPES.MemberExpression &&
+              node.init.argument.callee.object.type === AST_NODE_TYPES.ThisExpression &&
+              node.init.argument.callee.property.type === AST_NODE_TYPES.Identifier &&
+              node.init.argument.callee.property.name === 'parse' &&
+              node.id.type === AST_NODE_TYPES.ObjectPattern &&
+              node.id.properties.some(
+                (p) =>
+                  p.type === AST_NODE_TYPES.Property &&
+                  p.key.type === AST_NODE_TYPES.Identifier &&
+                  (p.key.name === 'args' || p.key.name === 'argv')
+              )
+            ) {
+              // Verify that the class has strict = false
+              const ancestors = context.getAncestors();
+              const sfCommand = getSfCommand(ancestors);
+              const strictProperty = sfCommand.body.body.find(
+                (p) =>
+                  p.type === AST_NODE_TYPES.PropertyDefinition &&
+                  ASTUtils.isIdentifier(p.key) &&
+                  p.key.name === 'strict'
+              );
+              if (
+                strictProperty?.type === AST_NODE_TYPES.PropertyDefinition &&
+                strictProperty.value.type === AST_NODE_TYPES.Literal &&
+                strictProperty.value.value === true
+              ) {
+                context.report({
+                  node: strictProperty,
+                  messageId: 'summary',
+                  fix: (fixer) => {
+                    return fixer.replaceText(strictProperty.value, 'false');
+                  },
+                });
+              } else if (!strictProperty) {
+                context.report({
+                  node: node.id,
+                  messageId: 'summary',
+                  fix: (fixer) => {
+                    return fixer.insertTextBefore(sfCommand.body.body[0], 'public static readonly strict = false;');
+                  },
+                });
+              }
+            }
+          },
+        }
+      : {};
+  },
+});
---
+++ test/rules/no-args-parse-without-strict-false.test.ts
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import path from 'path';
+import { ESLintUtils } from '@typescript-eslint/utils';
+import { noArgsParseWithoutStrictFalse } from '../../src/rules/no-args-parse-without-strict-false';
+
+const ruleTester = new ESLintUtils.RuleTester({
+  parser: '@typescript-eslint/parser',
+});
+
+ruleTester.run('noArgsParseWithoutStrictFalse', noArgsParseWithoutStrictFalse, {
+  valid: [
+    {
+      name: 'Parses as expected',
+      filename: path.normalize('src/commands/foo.ts'),
+      code: `
+export default class EnvCreateScratch extends SfCommand<ScratchCreateResponse> {
+  public static readonly strict = false;
+  public async run(): Promise<ScratchCreateResponse> {
+    const {flags, args, argv} = await this.parse(EnvCreateScratch);
+  }
+}
+`,
+    },
+    {
+      name: 'No args parsed',
+      filename: path.normalize('src/commands/foo.ts'),
+      code: `
+export default class EnvCreateScratch extends SfCommand<ScratchCreateResponse> {
+  public async run(): Promise<ScratchCreateResponse> {
+    const {flags} = await this.parse(EnvCreateScratch);
+  }
+}
+    `,
+    },
+    {
+      name: 'Not in commands dir',
+      filename: path.normalize('foo.ts'),
+      code: `
+export default class EnvCreateScratch extends SfCommand<ScratchCreateResponse> {
+  public async run(): Promise<ScratchCreateResponse> {
+    const {flags, args, argv} = await this.parse(EnvCreateScratch);
+  }
+}
+`,
+    },
+  ],
+  invalid: [
+    {
+      name: 'parses args/var without strict=false',
+      filename: path.normalize('src/commands/foo.ts'),
+      errors: [{ messageId: 'summary' }],
+      code: `
+export default class EnvCreateScratch extends SfCommand<ScratchCreateResponse> {
+  public async run(): Promise<ScratchCreateResponse> {
+    const {flags, args, argv} = await this.parse(EnvCreateScratch);
+  }
+}
+`,
+      output: `
+export default class EnvCreateScratch extends SfCommand<ScratchCreateResponse> {
+  public static readonly strict = false;public async run(): Promise<ScratchCreateResponse> {
+    const {flags, args, argv} = await this.parse(EnvCreateScratch);
+  }
+}
+`,
+    },
+    {
+      name: 'parses args/var and strict=true',
+      filename: path.normalize('src/commands/foo.ts'),
+      errors: [{ messageId: 'summary' }],
+      code: `
+export default class EnvCreateScratch extends SfCommand<ScratchCreateResponse> {
+  public static readonly strict = true;
+  public async run(): Promise<ScratchCreateResponse> {
+    const {flags, args, argv} = await this.parse(EnvCreateScratch);
+  }
+}
+`,
+      output: `
+export default class EnvCreateScratch extends SfCommand<ScratchCreateResponse> {
+  public static readonly strict = false;
+  public async run(): Promise<ScratchCreateResponse> {
+    const {flags, args, argv} = await this.parse(EnvCreateScratch);
+  }
+}
+`,
+    },
+  ],
+});
