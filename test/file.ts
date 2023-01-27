8e4c3e0d0e35715c906b0cf4a0b9e9be0e1f532d
mshanemc <shane.mclaughlin@salesforce.com>
2023-01-27 08:10:57 -0600

    docs: explain empty file

--- test/file.ts
+++ test/file.ts
@@ -4,3 +4,6 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
+// for tests that use the TS compiler, this file needs to exist.
+// see https://typescript-eslint.io/custom-rules/#testing-typed-rules
